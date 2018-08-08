256ae6b7306a69339c213bfd302b7edb639dbcb7
Teck Hock Ng <200349362@student.georgianc.on.ca>
2018-08-08 14:45:36 -0400

    trying upload file to S3

--- app.js
+++ app.js
@@ -6,8 +6,15 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var sassMiddleware = require('node-sass-middleware');
 var fs = require('fs');
+var multer = require('multer');
+var multerS3 = require('multer-s3');
+
 const aws = require('aws-sdk');
-// const S3_BUCKET = process.env.S3_BUCKET;
+aws.config.region = 'us-east-2';
+var s3 = new aws.S3();
+
+
+const S3_BUCKET = process.env.S3_BUCKET;
 
 require('dotenv').config({
   path: 'variables.env'
@@ -21,6 +28,21 @@ var profilesRouter = require('./routes/profiles');
 
 var app = express();
 app.use(fileUpload());
+
+//multer with Amazon S3
+var upload = multer({
+  storage: multerS3({
+    s3: s3,
+    bucket: 'final-mean-project',
+    acl: 'public-read',
+    metadata: function (req, file, cb) {
+      cb(null, {fieldName: file.fieldname});
+    },
+    key: function (req, file, cb) {
+      cb(null, Date.now()+file.originalname);
+    }
+  })
+})
 
 // use mongoose to connect to mongo
 var mongoose = require( 'mongoose' );
@@ -64,5 +86,10 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+//multer response
+app.post('/upload', upload.array('photos', 3), function(req, res, next) {
+  res.send('Successfully uploaded ' + req.files.length + ' files!')
+})
 
 module.exports = app;
--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -29,47 +29,53 @@ function readURL(input) {
 }
 
 //not fully configured Amazon S3 Bucket Config
-// (() => {
-//   document.getElementById("image").onchange = () => {
-//     const files = document.getElementById('image').files;
-//     const file = files[0];
-//     if(file == null){
-//       return alert('No file selected.');
-//     }
-//     getSignedRequest(file);
-//   };
-// })();
-//
-// function getSignedRequest(file){
-//   const xhr = new XMLHttpRequest();
-//   xhr.open('GET', `/sign-s3?file-name=${file.name}&file-type=${file.type}`);
-//   xhr.onreadystatechange = () => {
-//     if(xhr.readyState === 4){
-//       if(xhr.status === 200){
-//         const response = JSON.parse(xhr.responseText);
-//         uploadFile(file, response.signedRequest, response.url);
-//       }
-//       else{
-//         alert('Could not get signed URL.');
-//       }
-//     }
-//   };
-//   xhr.send();
-// }
-//
-// function uploadFile(file, signedRequest, url){
-//   const xhr = new XMLHttpRequest();
-//   xhr.open('PUT', signedRequest);
-//   xhr.onreadystatechange = () => {
-//     if(xhr.readyState === 4){
-//       if(xhr.status === 200){
-//         document.getElementById('profile-image').src = url;
-//         document.getElementById('image').value = url;
-//       }
-//       else{
-//         alert('Could not upload file.');
-//       }
-//     }
-//   };
-//   xhr.send(file);
-// }
+function initUpload(){
+      const files = document.getElementById('image').files;
+      const file = files[0];
+      if(file == null){
+        return alert('No file selected.');
+      }
+      getSignedRequest(file);
+    }
+
+function getSignedRequest(file){
+  const xhr = new XMLHttpRequest();
+  xhr.open('GET', `/sign-s3?file-name=${file.name}&file-type=${file.type}`);
+  xhr.onreadystatechange = () => {
+    if(xhr.readyState === 4){
+      if(xhr.status === 200){
+        const response = JSON.parse(xhr.responseText);
+        uploadFile(file, response.signedRequest, response.url);
+      }
+      else{
+        alert('Could not get signed URL.');
+        console.log();
+      }
+    }
+  };
+  xhr.send();
+}
+
+function uploadFile(file, signedRequest, url){
+  const xhr = new XMLHttpRequest();
+  xhr.open('PUT', signedRequest);
+  xhr.onreadystatechange = () => {
+    if(xhr.readyState === 4){
+      if(xhr.status === 200){
+        document.getElementById('preview').src = url;
+        document.getElementById('image').value = url;
+      }
+      else{
+        alert('Could not upload file.');
+      }
+    }
+  };
+  xhr.send(file);
+}
+
+/*
+    Bind listeners when the page loads.
+   */
+   (() => {
+       document.getElementById('image').onchange = initUpload;
+   })();
